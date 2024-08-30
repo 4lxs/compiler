9b1ee98470cb3cd8c5ff3f30f7bc5ff78cf09868
svl <66408983+4lxs@users.noreply.github.com>
2024-08-30 11:26:14 +0200

    template: see https://github.com/cpp-best-practices/cmake_template

---
+++ src/kaleidoscope/main.cpp
@@ -0,0 +1,10 @@
+// This file will be generated automatically when cur_you run the CMake
+// configuration step. It creates a namespace called `myproject`. You can modify
+// the source template at `configured_files/config.hpp.in`.
+#include <fmt/core.h>
+// #include <internal_use_only/config.hpp>
+
+int main () {
+  fmt::print("hello");
+  return 0;
+}
